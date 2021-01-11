55369e4ae03adc58d92c6da77251d284ca57946a
Berker Erol <berkererol@gmail.com>
2021-01-10 23:37:35 -0500

    implemented the skeleton for keeping the course data in the store.

--- src/app/services/course.store.ts
+++ src/app/services/course.store.ts
@@ -1,14 +1,43 @@
-import { Observable } from 'rxjs';
+import { MessagesService } from './../messages/messages.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Course, sortCoursesBySeqNo } from '../model/course';
-import { map } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
+import { LoadingService } from '../loading/loading.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CourseStore {
 
-    courses$: Observable<Course[]>;
+    private subject = new BehaviorSubject<Course[]>([]);
+
+    courses$: Observable<Course[]> = this.subject.asObservable();
+
+    constructor(
+        private http: HttpClient,
+        private loadingService: LoadingService,
+        private messages: MessagesService
+    ) {
+        this.loadAllCourses();
+    }
+
+    private loadAllCourses() {
+        const loadCourses$ = this.http.get<Course[]>('/api/courses')
+            .pipe(
+                map(response => response["payload"]),
+                catchError(err => {
+                    const message = "There was a problem loading courses."
+                    this.messages.showErrors(message);
+                    console.log(message, err);
+                    return throwError(err);
+                }),
+                tap(courses => this.subject.next(courses) )
+            );
+        this.loadingService.showLoaderUntilCompleted(loadCourses$)
+            .subscribe();
+    }
 
     filterByCategory(courseCategory: string): Observable<Course[]> {
         return this.courses$
